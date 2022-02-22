5ede325beb7995ec89e70786c5d45029f02eb9ba
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-02-22 19:11:00 +0330

    project initial setup
    iteration through  object list of tasks added

--- src/index.js
+++ src/index.js
@@ -2,19 +2,52 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icons8-drop-down-24.png';
 
-function component() {
-  const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+//   // Add the image to our existing div.
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+//   element.appendChild(myIcon);
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+const placeholder = document.getElementById('placeholder');
+const taskList = [
+  {
+    index: 0,
+    description: 'task1',
+    completed : false,
+  },
+  {
+    index: 1,
+    description: 'task2',
+    completed : false,
+  },
+  {
+    index: 2,
+    description: 'task3',
+    completed : false,
+  },
+];
+
+taskList.forEach((task) => {
+  const item = document.createElement('div');
+  item.classList.add('items');
+  item.setAttribute('id', task.index);
+  item.innerHTML = `
+  <div>
+  <input type="checkbox" id="check">
+  <label for="check">${task.description}</label>
+  </div>
+  <img src="${Icon}" alt="move">
+  `;
+  placeholder.appendChild(item);
+});
