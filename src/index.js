ce3d912a0543a42770f9265526cd08dd824a6459
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-02-22 20:03:04 +0330

    linter errors fixed

--- src/index.js
+++ src/index.js
@@ -1,40 +1,24 @@
+/* eslint-disable-next-line */
 import _ from 'lodash';
 import './style.css';
 import Icon from './icons8-drop-down-24.png';
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.classList.add('hello');
-
-//   // Add the image to our existing div.
-//   const myIcon = new Image();
-//   myIcon.src = Icon;
-
-//   element.appendChild(myIcon);
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
 
 const placeholder = document.getElementById('placeholder');
 const taskList = [
   {
     index: 0,
     description: 'task1',
-    completed : false,
+    completed: false,
   },
   {
     index: 1,
     description: 'task2',
-    completed : false,
+    completed: false,
   },
   {
     index: 2,
     description: 'task3',
-    completed : false,
+    completed: false,
   },
 ];
 
